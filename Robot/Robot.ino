28c4b91446b9f7886fb38880162c9eb89627a28b
lano78 <larsnordstrom@me.com>
2020-01-21 11:23:27 +0100

    Updated project template

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,13 +1,54 @@
 /*
  * Arduino projekt för EEDat. 
  */
+// Dessa måste finnas med i projektet.
+#include "secrets.h"
+#include "config.h"
 
-void setup() {
-  // put your setup code here, to run once:
+// Inkludera olika bibliotek
+#include "WiFi.h"
 
+/**
+ * Här definerar ni funktioner.
+ * Lägg till funktioner och namnge dom logiskt.
+ */
+void initPins();
+void blinkStatusLed(int del);
+
+/**
+* Setup
+*/
+void setup()
+{
+  Serial.begin(115200);
+  initPins()
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/**
+* loop
+*/
+void loop()
+{
+  blinkStatusLed(500)
+}
 
+/**
+*  Alla pins som skall ha output eller input
+*  Här lägger ni till de pins som ska fungera som input/output.
+*/
+void initPins()
+{
+  setPinMode(STATUS_LED, OUTPUT);
+}
+
+/**
+* Denna funktion används för att blink status_led som definertats i config.h
+* Modifiera ej denna funktion
+*/
+void blinkStatusLed(int del)
+{
+  digitalWrite(STATUS_LED, HIGH);
+  delay(del);
+  digitalWrite(STATUS_LED, LOW);
+  delay(del);
 }
