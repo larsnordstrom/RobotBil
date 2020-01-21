97b764a867e7277025225b5f1e9239eae2a6f360
lano78 <larsnordstrom@me.com>
2020-01-21 11:13:16 +0100

    Config file.

---
+++ Robot/config.h
@@ -0,0 +1,15 @@
+/*
+* config.h
+* 
+* Denna fil används för att definera olika pins mm.
+*/
+
+// Oled
+#define OLED_SCREEN_WIDTH 128
+#define OLED_SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
+// Led pin
+#define STATUS_LED 13
+
+// Motor pins
