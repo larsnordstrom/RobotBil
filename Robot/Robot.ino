3f68dd76ae3cfa95af412bf28b6b4219cdb478e8
Ture Danielsson <ture.danielsson@jgy.se>
2020-01-23 14:34:34 +0100

    added wifi conncet

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -4,23 +4,26 @@
 // Dessa måste finnas med i projektet.
 #include "secrets.h"
 #include "config.h"
-
 // Inkludera olika bibliotek
 #include "WiFiEsp.h"
+#include <SoftwareSerial.h>
+
 
 /**
  * Här definerar ni funktioner.
  * Lägg till funktioner och namnge dom logiskt.
  */
+SoftwareSerial Serial1(6, 7);
 void initPins();
 void blinkStatusLed(int del);
 void connectToWiFi(void);
+int status = WL_IDLE_STATUS;
 /**
 * Setup
 */
 void setup()
 {
-  Serial.begin(115200);
+ Serial.begin(115200);
   initPins();
   connectToWiFi();
 }
@@ -58,8 +61,48 @@ void blinkStatusLed(int del)
 * Denna funktion används för att ansluta till wifi.
 */
 void connectToWiFi(){
-    
+  Serial1.begin(9600);
 
+  WiFi.init(&Serial1);
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true);
+  }
+
+
+  while (status != WL_CONNECTED) Serial.begin(115200);
+ 
+
+  WiFi.init(&Serial1);
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true);
+  }
+
+
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+  }
+
+
+  Serial.println("You're connected to the network");
+  printWifiStatus();
+
+}
+void printWifiStatus() {
+
+
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Adress: ");
+  Serial.println(ip);
 
 }
 
